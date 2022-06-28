77d2ac7ea2bb77f5b59209379f52f2f628c4c240
benzkittisak <benzkittisak@stv.ac.th>
2022-06-28 17:16:14 +0700

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Kittisak Panluea on 28/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
