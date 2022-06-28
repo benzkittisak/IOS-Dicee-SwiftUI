1e9e403a58e5f7f4e4216196c53f22bb7d5fca95
benzkittisak <benzkittisak@stv.ac.th>
2022-06-28 18:01:04 +0700

    update project and code

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State เป็นตัวที่อนุญาตให้เราสามารถเปลี่ยนแปลงค่าในตัวแปรได้ เหมือนกับตัว mutating นั่นแหละ
+    @State var leftDiceNumber : Int = 1
+    @State var rightDiceNumber :Int = 2
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack() {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNumber:leftDiceNumber)
+                    DiceView(diceNumber:rightDiceNumber)
+                }
+                .padding(.all)
+                Spacer()
+                Button("Roll") {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }
+                .tint(.red)
+                .font(.system(size: 30))
+                .buttonStyle(.bordered)
+                .controlSize(.large)
+                .padding(.horizontal)
+            }
+        }
     }
 }
+
+struct DiceView: View {
+    
+    let diceNumber : Int
+    
+    var body: some View {
+        Image("dice\(diceNumber)")
+            .resizable()
+        //  1 หมายถึงให้ความกว้างกับสูงเท่ากันน่ะ
+            .aspectRatio(1 , contentMode: .fit)
+            .padding(30)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
